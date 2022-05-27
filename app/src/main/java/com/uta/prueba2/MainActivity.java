a0cd5163ca01fbc010634a8a48715ae2d0b2cf79
kevho <kevincaizaguano2@gmail.com>
2022-05-26 19:13:32 -0500

    commit 19:13

--- app/src/main/java/com/uta/prueba2/MainActivity.java
+++ app/src/main/java/com/uta/prueba2/MainActivity.java
@@ -2,12 +2,33 @@ package com.uta.prueba2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextUsuario, editTextContraseña;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextUsuario = findViewById(R.id.editTextUsuario);
+        editTextContraseña = findViewById(R.id.editTextContraseña);
+
+    }
+
+    public  void Ingresar (View view) {
+
+        String usuario = editTextUsuario.getText().toString();
+        String contraseña = editTextContraseña.getText().toString();
+
+        if (usuario.equals("admin") && contraseña.equals("admin")){
+            Intent intent = new Intent(this,MainActivity_1.class);
+            startActivity(intent);
+        }
     }
 }
